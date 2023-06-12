1603a9a5ae3bb299a54c038ef8fb5748c74578c4
Jamil Sinno <jamil.sinno@gmail.com>
2023-06-12 10:30:16 -0300

    rearranged file system

--- src/utils/cardData.js
+++ src/utils/cardData.js
@@ -2,7 +2,7 @@ const cardData = [
     {
         title: "UNUM Health",
         body: "Start up - Full Stack Application built with React, PHP, and PostgreSQL in Docker environment. Contract complete.",
-        img: "../../public/images/unumHealth.png",
+        img: "../../public/unumHealth.png",
         alt: "Unum Health Login Page",
         href: "#",
         repo: "No repository available",
@@ -10,7 +10,7 @@ const cardData = [
     {
         title: "No Game Left Behind",
         body: "Designed in HTML, CSS, JavaScript, EJS, Node.js, Express, Passport.js, Tailwind CSS, Daisy UI, MongoDB",
-        img: "../../public/images/Login.png",
+        img: "../../public/Login.png",
         alt: "Front page of No Game Left Behind",
         href: "#",
         repo: "https://github.com/jamilsinno/100hours",
@@ -18,7 +18,7 @@ const cardData = [
     {
         title: "R.A.M.P. Atlantic Ltd.",
         body: "Business Website - Civil Engineering Firm. Designed in HTML, CSS & JS",
-        img: "../../public/images/rampatl.png",
+        img: "../../public/rampatl.png",
         alt: "RAMP Atlantic Home Page",
         href: "https://rampatlantic.netlify.app/index.html",
         repo: "No repository available",
